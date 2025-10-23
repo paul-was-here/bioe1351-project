2537b9341044601eba30c4b1b609bd311cf56b57
paul-was-here <paul.g.kullmann@gmail.com>
2025-10-23 10:44:56 -0400

    cleaned up

--- ard_accelerometer/Ard_Accelerometer_Plot.m
+++ ard_accelerometer/Ard_Accelerometer_Plot.m
@@ -1,3 +1,12 @@
+%{
+Author: Paul Kullmann
+Modified: 10/23/25
+
+Purpose: Read and plot data saved from the serial reading of connected
+Arduino + accelerometer for preliminary project testing.
+Use Arduino_Accelerometer.py to read & save from serial port.
+%}
+
 filename = '~/desktop/acc_data.csv';
 %filename = '~/desktop/SpeedUpSlowDown.csv';
 
@@ -14,7 +23,7 @@ plot(data(:,1), data(:,3),'b-',DisplayName="Y Data")
 fs = 9600; %assume sampling rate = baud rate 9600
 wn = 100/(fs/2); % Sets cutoff frequency for filter
 [b,a] = butter(1,wn,'low'); % Creates butterworth filter
-filtered = filtfilt(b,a,data(:,3)); % Performs butterwoth filter operation
+filtered = filtfilt(b,a,data(:,3)); % Performs filter operation
 
 %filtered = bandpass(data(:,3),[1 25],fs);
 
