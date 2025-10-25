0e9a3cc9bb1766c7ad9b3135902b4a70ce0ba373
paul-was-here <paul.g.kullmann@gmail.com>
2025-10-25 12:03:22 -0400

    updated sampling rate

--- ard_accelerometer/Ard_accelerometer/Ard_accelerometer.ino
+++ ard_accelerometer/Ard_accelerometer/Ard_accelerometer.ino
@@ -1,15 +1,16 @@
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 
 void loop() {
+  int fs = 100;
   int sensor1 = analogRead(A0);
   int sensor2 = analogRead(A1);
+  float delay_time = 1000/fs;
 
-  // send the two values separated by a comma
   Serial.print(sensor1);
   Serial.print(",");
   Serial.println(sensor2);
 
-  delay(1); // small delay to keep things readable
+  delay(delay_time);
 }
