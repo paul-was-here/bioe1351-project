9fabe1409ad92027c41e001b8e85464e2c1ae2ef
paul-was-here <paul.g.kullmann@gmail.com>
2025-10-26 14:25:27 -0400

    moved files

--- arduino code/ard_heartrate_spo2/ard_heartrate_spo2.ino
+++ arduino code/ard_heartrate_spo2/ard_heartrate_spo2.ino
@@ -52,14 +52,14 @@ void setup()
 
   Serial.read();
 
-  byte ledBrightness = 120; //Options: 0=Off to 255=50mA
+  byte ledBrightness = 60; //Options: 0=Off to 255=50mA
   byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
   byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
-  byte sampleRate = 1000; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  byte sampleRate = 400; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
   int pulseWidth = 411; //Options: 69, 118, 215, 411
   int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
 
-  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
+  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange);
 }
 
 void loop()
