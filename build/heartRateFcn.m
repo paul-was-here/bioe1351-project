702296d36e6b87be6ae0da9b1b699813020086ac
paul-was-here <paul.g.kullmann@gmail.com>
2025-11-11 17:46:45 -0500

    clean up and add output bounds

--- build/heartRateFcn.m
+++ build/heartRateFcn.m
@@ -10,13 +10,10 @@ function hr = heartRateFcn(ts, ppg, d)
     % Algorithm Settings:
     noise_rejection_threshold =         2.5;   % in stdev's
     peak_detection_threshold =          1.5;   % in stdev's 
-    showPlot =                          false; % for debug         
+    showPlot =                          false; % for debug only      
     
     % Filter operations:
     filt = filtfilt(d,ppg);
-
-    %plot(ts, filt, 'k-');
-
 
     upper_lim = noise_rejection_threshold*std(filt);
     lower_lim = -1 * upper_lim;
@@ -24,16 +21,19 @@ function hr = heartRateFcn(ts, ppg, d)
     % Set ts to start at 0:
     ts = ts - ts(1);
 
+    % Seek out all windows of the filtered signal above the noise threshold
     above_lim_idxs = find(filt > upper_lim);
     below_lim_idxs = find(filt < lower_lim);
 
+    % Create a vertical vector of all out-of-bounds instances
     out_of_bounds_idxs = [above_lim_idxs; below_lim_idxs];
 
     if isempty(out_of_bounds_idxs)
         % Case 1: No data is out of bounds
         [~, pkids] = findpeaks(filt, 'MinPeakProminence', ...
             peak_detection_threshold*std(filt));
-        hr = 60/mean(diff(ts(pkids)));
+        pre_hr = 60/mean(diff(ts(pkids)));
+        hr = checkBounds(pre_hr);
         if showPlot == true
             plot(ts, filt, 'r-', LineWidth = 2);
             yline(noise_rejection_threshold*std(filt), 'r');
@@ -41,87 +41,34 @@ function hr = heartRateFcn(ts, ppg, d)
             yline(peak_detection_threshold*std(filt), 'k--');
             yline(-peak_detection_threshold*std(filt), 'k--');
         end
-
-        % Try FFT to compare even if no noise
-        %{
-        Fs = length(filt)/(ts(end)-ts(1));
-        %fprintf("Fs: %.2f", Fs)
-        N = length(filt);
-        Y = fft(filt);
-        P2 = abs(Y/N);
-        P1 = P2(1:N/2+1);
-        P1(2:end-1) = 2*P1(2:end-1);
-        f = Fs*(0:(N/2))/N;
-        
-        mask = (f > 0.25 & f < 4);
-        P1_filt = P1(mask);
-        f_filt = f(mask);
-
-        [~, max_idx] = max(P1_filt);
-        dominantFreq = f_filt(max_idx);
-        fft_hr = 60*dominantFreq;
-
-        figure(2);
-        plot(f_filt, P1_filt);
-
-        fprintf("\nFFT Method: %.2f", fft_hr);
-        figure(1);
-        %}
         return
     else
+        % Identify leftmost and rightmost noise event timepoints
         left_noise_lim = min(out_of_bounds_idxs);
         right_noise_lim = max(out_of_bounds_idxs);
-
         if ts(left_noise_lim) > 3
+            % Case 2: >3s of clean signal available on left side of some
+            % noise event
             ts = ts(1:left_noise_lim-30);
             filt = filt(1:left_noise_lim-30);
         elseif ts(end) - ts(right_noise_lim) > 3
+            % Case 3: >3s of clean signal available on right side of some
+            % noise event
             ts = ts(right_noise_lim+30:end);
             filt = filt(right_noise_lim+30:end);
         elseif ts(right_noise_lim) - ts(left_noise_lim) > 4
+            % Case 4: >4s of clean signal available in middle of two noise
+            % events
             ts = ts(left_noise_lim+1:right_noise_lim-1);
             filt = filt(left_noise_lim+1:right_noise_lim-1);
         else
-            hr = "No heart rate could be computed";
+            hr = "NaN";
             return
         end
             [~, pkids] = findpeaks(filt, 'MinPeakProminence', ...
                 peak_detection_threshold*std(filt));
-            hr = 60/mean(diff(ts(pkids))); % printed outside of function
-
-            try
-                % Try FFT method:
-
-                Fs = length(filt)/(ts(end)-ts(1));
-
-                %{
-                %fprintf("Fs: %.2f", Fs)
-                N = length(filt);
-                Y = fft(filt);
-                P2 = abs(Y/N);
-                P1 = P2(1:N/2+1);
-                P1(2:end-1) = 2*P1(2:end-1);
-                f = Fs*(0:(N/2))/N;
-                
-                mask = (f > 0.25 & f < 4);
-                P1_filt = P1(mask);
-                f_filt = f(mask);
-    
-                [~, max_idx] = max(P1_filt);
-                dominantFreq = f_filt(max_idx);
-                fft_hr = 60*dominantFreq;
-    
-                figure(2);
-                plot(f_filt, P1_filt);
-                %}
-            catch
-                disp("Some fft error");
-            end
-
-            figure(1);
-                
-            %fprintf("\nHR (fft method): %.2f\n", fft_hr);
-
+            pre_hr = 60/mean(diff(ts(pkids)));
+            hr = checkBounds(pre_hr);
             if showPlot == true
                 plot(ts, filt, 'r-', LineWidth = 2);
                 yline(noise_rejection_threshold*std(filt), 'r');
@@ -130,5 +77,17 @@ function hr = heartRateFcn(ts, ppg, d)
                 yline(-peak_detection_threshold*std(filt), 'k--');
             end
             return
+    end
+end
+
+%% Helper Functions:
+function hr = checkBounds(in)
+    % Check if heartrate is in reasonable bounds (btw 40 & 220)
+    % Returns the heart rate if within bounds or NaN if out of bounds.
+
+    if in > 220 || in < 40
+        hr = "NaN";
+    else
+        hr = in;
     end
 end
