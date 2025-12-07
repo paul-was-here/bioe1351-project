19194be7deaa682c92e25ccdd14e04a8fe2cfa19
paul-chtlpc <pgk18@pitt.edu>
2025-12-07 14:41:45 -0500

    remove outdated code

--- build/cadenceFcn.m
+++ build/cadenceFcn.m
@@ -17,40 +17,8 @@ function cadence = cadenceFcn(ts, acc)
         cadence = 60/mean(diff(ts(pkids)));
         return
     elseif detection_method == "fft"
-        % i don't think the fft method was written correctly- it is updated
-        % in another fcn within the app itself anyways so this is probably
-        % obsolete.
-        %
-        %
-        % Variables setup:
-        L = length(acc);
-        fs = length(acc)/(ts(end)-ts(1)); 
-
-        % Compute FFT:
-        Y = fft(acc);
-        P2 = abs(Y/L);          % Two-sided FFT
-        P1 = P2(1:floor(L/2)+1);       % One-sided FFT
-        f = fs * (0:(L/2)) / L; % Frequency vector
-
-        % Window FFT to relevant frequencies
-        [~, min_freq_idx] = min(abs(f - 0.5));
-        [~, max_freq_idx] = min(abs(f-5));
-        f_window = f(min_freq_idx:max_freq_idx);    % Adjust to frequency window of interest
-        amp_window = P1(min_freq_idx:max_freq_idx);
-
-        [sorted_amp, sort_idx] = sort(amp_window, 'descend');
-        sorted_freq = f_window(sort_idx);
-
-        top_freqs = sorted_freq(1:2);
-        top_amps = sorted_amp(1:2);
-
-        cadence = 60*top_freqs(1); % Assume most prominent frequency is the step
         
-        % Assume second most prominent frequency in y is up-down bobbing
-        if top_amps(2)/top_amps(1) > 0.5
-            %fprintf("\nBobbing up and down detected: Attempt to reduce" + ...
-            %    "bobbing motion.")
-        end
-
+        % written incorrectly and depracated. fft-based accelerometry
+        % analysis is in another function within the app itself
     end
 end
